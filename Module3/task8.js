fb8f5fdcb18de01d1cb26bcd96ec900b212c552e
Oreoluwa Hammed <hammedzainny@gmail.com>
2023-10-25 06:40:36 +0100

    for loop task

--- Module3/task8.js
+++ Module3/task8.js
@@ -1,1 +1,11 @@
 //conditional operator loops
+for (let number=1; number<=20; number++)
+{
+   // if the condition is true print odd numbers
+    if (number%2!==0)
+    {
+        // print all odd numbers
+        console.log(" " +number)
+    }
+   
+}
