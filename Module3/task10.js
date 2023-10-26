9f2eff6af2087149e3d87212f6c056a2fd0b094c
Oreoluwa Hammed <hammedzainny@gmail.com>
2023-10-26 08:56:00 +0100

    functions2

--- Module3/task10.js
+++ Module3/task10.js
@@ -1,18 +1,9 @@
-function fizzBuzz(n) {
-    for (let i = 1; i <= n; i++) {
-      if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
-      } else if (i % 3 === 0) {
-        console.log("Fizz");
-      } else if (i % 5 === 0) {
-        console.log("Buzz");
-      } else {
-        console.log(i);
-      }
-    }
+function calculateAreaOfRectangle(length, width) {
+    const area = length * width;
+    return area;
   }
   
-  // Example usage:
-  const n = 20;
-  fizzBuzz(n);
+
+  const area = calculateAreaOfRectangle(10, 14);
+  console.log("The area of the rectangle is: " + area);
   
