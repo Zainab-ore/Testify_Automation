caa5cc924a0b831733e6615683f28493c1556a4f
Oreoluwa Hammed <hammedzainny@gmail.com>
2023-10-26 08:20:19 +0100

    functions

---
+++ Module3/task10.js
@@ -0,0 +1,18 @@
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+      } else if (i % 3 === 0) {
+        console.log("Fizz");
+      } else if (i % 5 === 0) {
+        console.log("Buzz");
+      } else {
+        console.log(i);
+      }
+    }
+  }
+  
+  // Example usage:
+  const n = 20;
+  fizzBuzz(n);
+  
--- Module3/task8.js
+++ Module3/task8.js
@@ -5,7 +5,9 @@ for (let number=1; number<=20; number++)
     if (number%2!==0)
     {
         // print all odd numbers
-        console.log(" " +number)
+        console.log(number+ " is an odd number")
     }
-   
+   else{
+    console.log(number+ " is an even number")
+   }
 }
