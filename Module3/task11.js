8540b0fc7269cfeff969dfbe60d9617e387796f2
Oreoluwa Hammed <hammedzainny@gmail.com>
2023-10-27 11:13:47 +0100

    ARRAYs

---
+++ Module3/task11.js
@@ -0,0 +1,3 @@
+//mArray of top 5friends
+const friends = ["ME", "ARI", "LIZZY", "MYSELF", "ZAINAB"]
+console.log('These are my top 5 friends: ' +friends);
---
+++ Module3/task12.js
@@ -0,0 +1,5 @@
+//pick your best friend
+const friends = ["ME", "ARI", "LIZZY", "MYSELF", "ZAINAB"]//from5 of my friends
+
+const bestFriend=friends[4] //i choose my bestie zainab
+console.log('My bestie is: ' +bestFriend); //this should pick ZAINAB
