843ba1f497296655a12d0ab21921b2d3bdba14dc
Oreoluwa Hammed <hammedzainny@gmail.com>
2023-12-05 15:44:08 +0100

    task

---
+++ Module4/Task/Task2/DifferentDataTypes.java
@@ -0,0 +1,19 @@
+public class DifferentDataTypes {
+    public static void main(String[] args) {
+        // Declare variables of different data types
+        int integerVariable = 99;
+        double doubleVariable = 99.99;
+        char charVariable = 'A';
+        boolean booleanVariable = false;
+        long longVariable = 1234567890L;
+        String stringVariable = "Programming in Java!";
+
+        // Prints all the values of the variables on each line
+        System.out.println("Integer Variable: " + integerVariable);
+        System.out.println("Double Variable: " + doubleVariable);
+        System.out.println("Char Variable: " + charVariable);
+        System.out.println("Boolean Variable: " + booleanVariable);
+        System.out.println("Long Variable: " + longVariable);
+        System.out.println("String Variable: " + stringVariable);
+    }
+}
