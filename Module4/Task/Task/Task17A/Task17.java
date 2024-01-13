5e7735731fcf330cacbbee895f1779b659ac96fd
Oreoluwa Hammed <hammedzainny@gmail.com>
2024-01-13 04:54:10 +0100

    p

--- Module4/Task/Task/Task17A/Task17.java
+++
@@ -1,5 +0,0 @@
-package Task.Task.Task17A;
-
-public class Task17 {
-
-}
--- Module4/Task/Task/Task17B/Logintest.java
+++
@@ -1,9 +0,0 @@
-package Task.Task.Task17B;
-
-
-public interface Logintest {
-    public void testUsername(String username);
-    public void testPassword(String password);
-
-    public void testSuccessfulLogin(String username, String password);
-}
