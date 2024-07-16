b980e8f5d021c0fd264c0978a91c9d957faed406
Oreoluwa Hammed <hammedzainny@gmail.com>
2024-07-16 01:05:55 +0100

    .

--- Module4/Module4C_PYTHON/Python_Task/Task20.py
+++ Module4/Module4C_PYTHON/Python_Task/Task20.py
@@ -2,7 +2,7 @@
 task:
 Create a class called Human
 
-Add an attribute leg_count with the value of 4
+Add an attribute leg_count with the value of 4.
 
 Add a method called get_gender() that returns "Unknown" in the Human class
 
